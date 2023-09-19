e29b4773ba91c63d8314951b73b42dcbdfdce122
ashish9456 <129813971+ashish9456@users.noreply.github.com>
2023-09-19 15:08:08 +0530

    Add files via upload

---
+++ NPTEL Assignments/Week 8/Pgm_Asgn_4.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+void reverse(char[], int, int);
+    int main()
+    {
+        char str1[20];
+        scanf("%s", str1); //The string is taken as input form the test data. 
+     
+/* Complete the program to print the string in reverse order using the function
+void reverse(char[], int, int);
+Use the printf statement as
+printf("The string after reversing is: %s\n", str1); 
+You can use different variable also.
+*/
+    int len = strlen(str1);
+    reverse(str1, 0, len - 1);
+    printf("The string after reversing is: %s", str1);
+    return 0;
+}
+
+void reverse(char str[], int start, int end) 
+{
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
