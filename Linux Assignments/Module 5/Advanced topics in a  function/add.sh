b32154bf623e01de2a89c3576718e5379304437b
Ashish <129813971+ashish9456@users.noreply.github.com>
2023-10-06 17:14:26 +0530

    Add files via upload

---
+++ Linux Assignments/Module 5/Advanced topics in a  function/add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+add()
+{
+	result=$(($1+$2))
+	echo "The sum of $1 and $2 = $result"
+}
+
+
---
+++ Linux Assignments/Module 5/Advanced topics in a  function/call.sh
@@ -0,0 +1,6 @@
+# call.sh
+
+echo "Add.sh is called in call.sh"
+source add.sh 
+add 10 15
+
